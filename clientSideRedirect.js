c5b2ed8d068c20ffd09d291ab87176707a9693df
Brent Larson <taulars@gmail.com>
2020-02-10 12:39:52 -0700

    Remove trailing slash
    Remove trailing slash to support hash & query string redirects

--- clientSideRedirect.js
+++ clientSideRedirect.js
@@ -4,13 +4,19 @@ module.exports = function getMetaRedirect(toPath) {
   const hasProtocol = url.includes('://');
   if (!hasProtocol) {
     const hasLeadingSlash = url.startsWith('/');
+    const hasTrailingSlash = url.endsWith('/');
+    
     if (!hasLeadingSlash) {
       url = `/${url}`;
+    }
+    
+    if(hasTrailingSlash) {
+      url = url.slice(0, -1);
     }
 
     const resemblesFile = url.includes('.');
     if (!resemblesFile) {
-      url = `${url}/`.replace(/\/\/+/g, '/');
+      url = `${url}`.replace(/\/\/+/g, '/');
     }
   }
 
